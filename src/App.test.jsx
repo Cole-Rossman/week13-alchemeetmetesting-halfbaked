3a6c88117d62196bae38248058ea576124e5d484
Cole Rossman <colerossman99@gmail.com>
2022-04-25 15:45:15 -0700

    Header test passes image and profile name

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,12 @@
+import { screen, render } from '@testing-library/react';
+import App from './App';
 
-test('Should render the header', () => {
+test('Should render the header', async () => {
+    render(<App />)
 
+    const profileName = await screen.findByText(/vonta/i);
+    const headerImage = screen.getByAltText(/alchemy/i);
+   
+    expect(profileName).toBeInTheDocument();
+    expect(headerImage).toBeInTheDocument();
 })
